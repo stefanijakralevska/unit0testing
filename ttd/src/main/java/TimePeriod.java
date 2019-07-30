ea494baa443462e236e309145958bc94df053066
Stefanija Kralevska <Stefanija.Kralevska@endava.com>
2019-07-30 17:54:15 +0200

    Unit tests for TimePeriod & some modifications in TimePeriod class

--- ttd/src/main/java/TimePeriod.java
+++ ttd/src/main/java/TimePeriod.java
@@ -12,36 +12,53 @@ public class TimePeriod {
     public boolean overlapsWith(TimePeriod period) {
         // testAcontainsB()
         if ((this.start.before(period.start))
-            && (this.end.after(period.end))) {
+                && (this.end.after(period.end))) {
             return true;
         }
         // testBcontainsA()
 
         if ((this.start.after(period.start))
-            && (this.end.before(period.end))) {
+                && (this.end.before(period.end))) {
             return true;
         }
+
         // testBinteractA()
         if ((this.start.after(period.start))
-            && (this.end.after(period.end))) {
+                && (this.end.after(period.end))
+                && (isPeriodEndBetween(period))) {
             return true;
         }
+
         // testAinteractB()
         if ((this.start.before(period.start))
-            && (this.end.before(period.end))) {
+                && (this.end.before(period.end))
+                && (isPeriodStartBetween(period))) {
             return true;
         }
+
+
         // testAequalsB()
         if ((this.start.equals(period.start))
-            && (this.end.equals(period.end))) {
+                && (this.end.equals(period.end))) {
             return true;
         }
         // testAendEqualToBstart()
         if (this.end.equals(period.start)) {
             return true;
         }
+        // testBendEqualToAstart()
+        if(this.start.equals(period.end)) {
+            return true;
+        }
 
         return false;
     }
 
+    private boolean isPeriodEndBetween(TimePeriod timePeriod) {
+        return timePeriod.end.after(this.start) && timePeriod.end.before(this.end);
+    }
+
+    private boolean isPeriodStartBetween(TimePeriod timePeriod) {
+        return timePeriod.start.after(this.start) && timePeriod.start.before(this.end);
+    }
 }
--- ttd/src/test/java/TimePeriodTest.java
+++ ttd/src/test/java/TimePeriodTest.java
@@ -1,10 +1,78 @@
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 
 public class TimePeriodTest {
 
-    @Test
-    public void overlapsWith() {
+    private  TimePeriod timePeriodA;
+    SimpleDateFormat dateFormat;
+    TimePeriod timePeriodB;
+
+    @Before
+    public void setUp() throws ParseException {
+        dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        timePeriodA = new TimePeriod(dateFormat.parse("01/01/2019"), dateFormat.parse("30/04/2019"));
     }
+
+    @Test
+    public void testAcontainsB() throws ParseException {
+
+        timePeriodB = new TimePeriod(dateFormat.parse("01/02/2019"), dateFormat.parse("28/02/2019"));
+        Assert.assertTrue(timePeriodA.overlapsWith(timePeriodB));
+
+    }
+
+    @Test
+    public void testBcontainsA() throws ParseException {
+
+        timePeriodB = new TimePeriod(dateFormat.parse("01/01/2018"), dateFormat.parse("30/04/2020"));
+        Assert.assertTrue(timePeriodA.overlapsWith(timePeriodB));
+
+    }
+
+    @Test
+    public void testBinteractA() throws ParseException {
+        timePeriodB = new TimePeriod(dateFormat.parse("01/02/2018"), dateFormat.parse("01/05/2019"));
+        Assert.assertTrue(timePeriodA.overlapsWith(timePeriodB));
+    }
+
+    @Test
+    public void testAinteractB() throws ParseException {
+        timePeriodB = new TimePeriod(dateFormat.parse("01/02/2019"),dateFormat.parse("01/06/2019"));
+        Assert.assertTrue(timePeriodA.overlapsWith(timePeriodB));
+    }
+
+
+    @Test
+    public void testAequalsB() throws ParseException {
+        timePeriodB = timePeriodA;
+        Assert.assertTrue(timePeriodA.overlapsWith(timePeriodB));
+    }
+
+    @Test
+    public void testAendEqualToBstart() throws ParseException {
+        timePeriodB =  new TimePeriod(dateFormat.parse("30/04/2019"),dateFormat.parse("01/06/2019"));
+        Assert.assertTrue(timePeriodA.overlapsWith(timePeriodB));
+    }
+
+
+    @Test
+    public void testBendEqualToAstart() throws ParseException {
+        timePeriodB = new TimePeriod(dateFormat.parse("01/06/2018"), dateFormat.parse("01/01/2019"));
+        Assert.assertTrue(timePeriodA.overlapsWith(timePeriodB));
+    }
+
+    @Test
+    public void testAnotOverlapB() throws ParseException {
+        timePeriodB = new TimePeriod(dateFormat.parse("01/01/2018"), dateFormat.parse("30/04/2018"));
+        Assert.assertFalse(timePeriodA.overlapsWith(timePeriodB));
+
+    }
+
+
+
 }
